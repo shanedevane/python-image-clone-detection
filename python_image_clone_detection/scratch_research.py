ac9b148e5c1db186f092e84125cfe8e6dd15cf12
Shane Devane (SD-WIN) <shanedevane@gmail.com>
2016-09-30 16:42:38 +0100

    method to split image into blocks with output debug

--- python_image_clone_detection/scratch_research.py
+++ python_image_clone_detection/scratch_research.py
@@ -14,7 +14,8 @@ from decimal import Decimal
 
 
 class ImageCloneDetector:
-    IMAGE_BLOCKS = 15
+    IMAGE_BLOCKS = 200
+    SAVE_BLOCK_IMAGES_OUTPUT = True
 
     def __init__(self, file_path):
         self._file_path = file_path
@@ -29,49 +30,65 @@ class ImageCloneDetector:
         duplicate_blocks = list()
         for outer_block in self._blocks:
             for inner_block in self._blocks:
-                if outer_block == inner_block:
-                    duplicate_blocks.append(outer_block)
+
+                pass
+                # if outer_block == inner_block:
+                #     duplicate_blocks.append(outer_block)
 
         return duplicate_blocks
-     #
-     #
-     #
-     #    l = len(imagparts[0])-1
-     #
-     # for i in range(len(imagparts)-1):
-     #  difs = sum(abs(x-y) for x,y in zip(imagparts[i][:l],imagparts[i+1][:l]))
-     #  mean = float(sum(imagparts[i][:l])) / l
-     #  dev = float(sum(abs(mean-val) for val in imagparts[i][:l])) / l
-     #  if dev/mean >= float(opt.blcoldev):
-     #   if difs <= int(opt.blsim):
-     #    if imagparts[i] not in dupl:
-     #     dupl.append(imagparts[i])
-     #    if imagparts[i+1] not in dupl:
-     #     dupl.append(imagparts[i+1])
-     #
-     # return dupl
 
     def _split_up_image_into_blocks(self):
         blocks = list()
         image_width, image_height = self._grey_image.size
-        # image = self._grey_image.load()
+        image_width += ImageCloneDetector.IMAGE_BLOCKS
+        image_height += ImageCloneDetector.IMAGE_BLOCKS
 
-        for x in range(0, image_width - ImageCloneDetector.IMAGE_BLOCKS, ImageCloneDetector.IMAGE_BLOCKS):
-            for y in range(0, image_height - ImageCloneDetector.IMAGE_BLOCKS, ImageCloneDetector.IMAGE_BLOCKS):
-
-                block_top = x * ImageCloneDetector.IMAGE_BLOCKS
-                block_left = y * ImageCloneDetector.IMAGE_BLOCKS
-                block_right = x + ImageCloneDetector.IMAGE_BLOCKS
+        for x in range(0, image_width, ImageCloneDetector.IMAGE_BLOCKS):
+            for y in range(0, image_height, ImageCloneDetector.IMAGE_BLOCKS):
+                block_top = y
+                block_left = x
                 block_bottom = y + ImageCloneDetector.IMAGE_BLOCKS
-                block = (block_top, block_left, block_right, block_bottom)
-                blocks.append(self._grey_image.crop(block))
+                block_right = x + ImageCloneDetector.IMAGE_BLOCKS
+
+                block = (block_left, block_top, block_right, block_bottom)
+                image_block = self._grey_image.crop(block)
+
+                if ImageCloneDetector.SAVE_BLOCK_IMAGES_OUTPUT:
+                    try:
+                        image_block.save('../output/{0}.{1}x{2}.{3}.jpg'
+                                         .format(block_top, block_left, block_bottom, block_right))
+                    except SystemError as e:
+                        pass
+
+
+        # for y in range(0, image_height + ImageCloneDetector.IMAGE_BLOCKS, ImageCloneDetector.IMAGE_BLOCKS):
+
+
+            # for x in range(0, image_width + ImageCloneDetector.IMAGE_BLOCKS, ImageCloneDetector.IMAGE_BLOCKS):
+            #     block_top = x
+            #     block_left = y
+            #     block_bottom = x + ImageCloneDetector.IMAGE_BLOCKS
+            #     block_right = y + ImageCloneDetector.IMAGE_BLOCKS
+            #
+            #     # left, top, right, bottom
+            #     block = (block_left, block_top, block_right, block_bottom)
+            #     image_block = self._grey_image.crop(block)
+            #     # print(image_block)
+            #     try:
+            #         image_block.save('../output/{0}.{1}x{2}.{3}.jpg'.format(block_top, block_left, block_bottom, block_right))
+            #     except SystemError as e:
+            #         pass
+            #     blocks.append(image_block)
+
+
                 # parts.append(image[x:x+ImageCloneDetector.IMAGE_BLOCKS, y:y+ImageCloneDetector.IMAGE_BLOCKS])
 
         # parts = sorted(parts)
         return blocks
 
     def execute(self):
-        self._grey_image = Image.open(self._file_path).convert('L')
+        # self._grey_image = Image.open(self._file_path).convert('L')
+        self._grey_image = Image.open(self._file_path)
         self._blurred_image = self._grey_image.filter(ImageFilter.SMOOTH_MORE)
         self._blocks = self._split_up_image_into_blocks()
         duplicates = self._compare_blocks()
